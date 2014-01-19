94bc0e69978f1fd87ce10953002312b2f04c7eda
MarcinCz <marcinczrw@gmail.com>
2014-01-19 21:24:56 +0100

    Testy do algorytmu.

--- ealib-tests/EvolutionaryAlgorithmTest.h
+++ ealib-tests/EvolutionaryAlgorithmTest.h
@@ -15,8 +15,13 @@ public:
 	{
 		CppUnit::TestSuite *suite = new CppUnit::TestSuite("PopulationTests");
 		suite->addTest(new CppUnit::TestCaller<EvolutionaryAlgorithmTest>(
-			"doSelectionRankingCPUTest", &EvolutionaryAlgorithmTest::runAlgorithmNoCrossoverNormalCPUTest));
-
+			"runAlgorithmNoCrossoverNormalCPUTest", &EvolutionaryAlgorithmTest::runAlgorithmNoCrossoverNormalCPUTest));
+		suite->addTest(new CppUnit::TestCaller<EvolutionaryAlgorithmTest>(
+			"runAlgorithmWithCrossoverNormalCPUTest", &EvolutionaryAlgorithmTest::runAlgorithmWithCrossoverNormalCPUTest));
+		suite->addTest(new CppUnit::TestCaller<EvolutionaryAlgorithmTest>(
+			"runAlgorithmNoCrossoverUniformCPUTest", &EvolutionaryAlgorithmTest::runAlgorithmNoCrossoverUniformCPUTest));
+		suite->addTest(new CppUnit::TestCaller<EvolutionaryAlgorithmTest>(
+			"runAlgorithmWithCrossoverUniformCPUTest", &EvolutionaryAlgorithmTest::runAlgorithmWithCrossoverUniformCPUTest));
 		return suite;
 
 	}
@@ -30,7 +35,7 @@ public:
 	{
 		ealib::eaoperator::NormalMutationOperatorPtr mutation = ealib::CPUObjectFactory::getInstance().getNormalMutationOperator(2,2);
 		ealib::SelectionPtr selection = ealib::CPUObjectFactory::getInstance().getSelection(ealib::Selection::RANKIG);
-		ealib::StopCondition stop_condition = ealib::StopCondition(1000, 5);
+		ealib::StopCondition stop_condition = ealib::StopCondition(1000, 4);
 		ealib::EvolutionaryAlgorithm algorithm = ealib::EvolutionaryAlgorithm(pop,
 																		stop_condition,
 																		boost::bind(&EvolutionaryAlgorithmTest::myFitnessFunction, this, _1),
@@ -38,7 +43,60 @@ public:
 																		mutation);
 
 		ealib::Result result = algorithm.run();
-		CPPUNIT_ASSERT(result.getBestIndividual().getFitnessValue() > 2);
+		CPPUNIT_ASSERT_MESSAGE("Try to test again if it fails.", result.getBestIndividual().getFitnessValue() > 4);
+		CPPUNIT_ASSERT(result.getStepsTaken() <= 1000);
+	}
+
+	void runAlgorithmWithCrossoverNormalCPUTest()
+	{
+		ealib::eaoperator::NormalMutationOperatorPtr mutation = ealib::CPUObjectFactory::getInstance().getNormalMutationOperator(2,2);
+		ealib::eaoperator::CrossoverOperatorPtr crossover = ealib::CPUObjectFactory::getInstance().getCrossoverOperator(0.1);
+		ealib::SelectionPtr selection = ealib::CPUObjectFactory::getInstance().getSelection(ealib::Selection::PROPORTIONAL);
+		ealib::StopCondition stop_condition = ealib::StopCondition(1000, 4);
+		ealib::EvolutionaryAlgorithm algorithm = ealib::EvolutionaryAlgorithm(pop,
+																		stop_condition,
+																		boost::bind(&EvolutionaryAlgorithmTest::myFitnessFunction, this, _1),
+																		selection,
+																		mutation,
+																		crossover);
+
+		ealib::Result result = algorithm.run();
+		CPPUNIT_ASSERT_MESSAGE("Try to test again if it fails.", result.getBestIndividual().getFitnessValue() > 4);
+		CPPUNIT_ASSERT(result.getStepsTaken() <= 1000);
+	}
+
+	void runAlgorithmNoCrossoverUniformCPUTest()
+	{
+		ealib::eaoperator::UniformMutationOperatorPtr mutation = ealib::CPUObjectFactory::getInstance().getUniformMutationOperator(3,2);
+		ealib::SelectionPtr selection = ealib::CPUObjectFactory::getInstance().getSelection(ealib::Selection::RANKIG);
+		ealib::StopCondition stop_condition = ealib::StopCondition(1000, 4);
+		ealib::EvolutionaryAlgorithm algorithm = ealib::EvolutionaryAlgorithm(pop,
+																		stop_condition,
+																		boost::bind(&EvolutionaryAlgorithmTest::myFitnessFunction, this, _1),
+																		selection,
+																		mutation);
+
+		ealib::Result result = algorithm.run();
+		CPPUNIT_ASSERT_MESSAGE("Try to test again if it fails.", result.getBestIndividual().getFitnessValue() > 4);
+		CPPUNIT_ASSERT(result.getStepsTaken() <= 1000);
+	}
+
+	void runAlgorithmWithCrossoverUniformCPUTest()
+	{
+		ealib::eaoperator::UniformMutationOperatorPtr mutation = ealib::CPUObjectFactory::getInstance().getUniformMutationOperator(3,2);
+		ealib::eaoperator::CrossoverOperatorPtr crossover = ealib::CPUObjectFactory::getInstance().getCrossoverOperator(0.1);
+		ealib::SelectionPtr selection = ealib::CPUObjectFactory::getInstance().getSelection(ealib::Selection::RANKIG);
+		ealib::StopCondition stop_condition = ealib::StopCondition(1000, 4);
+		ealib::EvolutionaryAlgorithm algorithm = ealib::EvolutionaryAlgorithm(pop,
+																		stop_condition,
+																		boost::bind(&EvolutionaryAlgorithmTest::myFitnessFunction, this, _1),
+																		selection,
+																		mutation,
+																		crossover);
+
+		ealib::Result result = algorithm.run();
+		CPPUNIT_ASSERT_MESSAGE("Try to test again if it fails.", result.getBestIndividual().getFitnessValue() > 4);
+		CPPUNIT_ASSERT(result.getStepsTaken() <= 1000);
 	}
 
 	void setUp()
--- ealib/Result.h
+++ ealib/Result.h
@@ -9,21 +9,21 @@ namespace ealib {
 	class Result
 	{
 	public:
-		Result(int _steps_number, double _running_time, Individual& _best_individual)
+		Result(int _steps_taken, double _running_time, Individual& _best_individual)
 		{
-			steps_number = _steps_number;
+			steps_taken = _steps_taken;
 			running_time = _running_time;
 			best_individual = &_best_individual;
 		}
 		~Result() {};
 		Individual getBestIndividual() { return *best_individual; }
-		int getStepsNumber() const { return steps_number; }
+		int getStepsTaken() const { return steps_taken; }
 		///Returns algorithm's running time in seconds.
 		double getRunningTime() const { return running_time; }
 
 	private:
 		Individual *best_individual;
-		int steps_number;
+		int steps_taken;
 		double running_time;
 	};
 
